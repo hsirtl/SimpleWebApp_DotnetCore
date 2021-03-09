9e7d1d6899e95c49978e3e6eac3d60141a97e04f
Holger Sirtl <holger.sirtl@hotmail.com>
2021-03-09 10:23:21 +0100

    add deployment script

--- deploy/prepareResourceGroup.sh
+++
@@ -1,6 +0,0 @@
-# if [ $(az group exists --name $RESOURCEGROUPNAME) = false ]; then
-#    az group create --name $RESOURCEGROUPNAME --location $LOCATION
-# fi
-if [ $(az group exists --name 'ArchitectsConnect') = false ]; then
-    az group create --name 'ArchitectsConnect' --location 'westeurope'
-fi
